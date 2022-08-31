7a6c936fbbfc57ac45c11ba66ba3a37b0e73ba04
canguejamba <canguejamba@gmail.com>
2022-08-31 22:49:18 +0200

    update readme.md

--- comment/src/main/java/io/blog/comment/CommentController.java
+++ comment/src/main/java/io/blog/comment/CommentController.java
@@ -9,9 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
